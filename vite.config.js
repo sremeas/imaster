8389fe5f100646a5bc777b7d4870d2669fe23795
Kakratana <39192742+Kakratana@users.noreply.github.com>
2026-01-14 23:45:51 +0700

    Update base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/imaster/', // <--- ADD THIS LINE (use your actual repo name)
+  base: '/', // <--- ADD THIS LINE (use your actual repo name)
 })
